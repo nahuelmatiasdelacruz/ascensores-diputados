d47432a65cd968bc13b0f6916c55b257cc3e412f
Nahuel De La Cruz <84105610+nahuelmatiasdelacruz@users.noreply.github.com>
2023-09-18 23:26:18 -0300

    Update config.js

--- src/db/config.js
+++ src/db/config.js
@@ -2,9 +2,9 @@ module.exports = {
     development: {
       client: 'pg',
       connection: {
-        host: "200.58.123.214",
-        user: "sgp_user",
-        password: "Us3r_SGP_432%",
+        host: "process.env.DBHOST",
+        user: "process.env.DBUSER",
+        password: "process.env.DBPASSWORD",
         database: "sgp_desa",
         port: 5432,
       }
